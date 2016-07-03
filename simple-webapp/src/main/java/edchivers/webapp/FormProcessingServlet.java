3770a1b188e52279c69dd67cfb23c5b8ac545281
ed <ed@geekmachine>
2016-07-03 18:02:41 +0100

    Updated servlet to use validator - returns simple message

--- simple-webapp/src/main/java/edchivers/webapp/FormProcessingServlet.java
+++ simple-webapp/src/main/java/edchivers/webapp/FormProcessingServlet.java
@@ -4,15 +4,41 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import edchivers.webapp.validators.StringArrayDataValidator;
+
 import java.io.IOException;
 
 import javax.servlet.ServletException;
 
 public class FormProcessingServlet extends HttpServlet {
 	
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+	private StringArrayDataValidator dataValidator = new StringArrayDataValidator();
+	
+	public static final String FIRST_NAME_PARAM = "people[][firstname]";
+	public static final String LAST_NAME_PARAM = "people[][surname]";
+	
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp)
             throws ServletException, IOException
         {
-    		System.out.println("Servlet called!");
+    		if (validateFormData(req))
+    		{
+    			//TODO write data
+    			resp.getWriter().print("Thank you for your data");
+    			resp.getWriter().close();
+    		}
+    		else
+    		{
+    			resp.getWriter().print("Data was not valid");
+    			resp.getWriter().close();
+    		}
         }
+
+	private boolean validateFormData(HttpServletRequest req) {
+
+		String[] firstNames = req.getParameterValues(FIRST_NAME_PARAM);
+		String[] lastNames = req.getParameterValues(LAST_NAME_PARAM);
+		
+		return dataValidator.validate(firstNames, lastNames);
+
+	}
 }
